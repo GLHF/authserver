c570df4e510428434a4fdfcf6f7c68e0e6b66c8e
Timm <timur.galiakberov@gost-group.com>
2020-03-28 13:11:46 +0300

    Initial fix commit

--- src/main/java/ru/kpfu/synergy/authserver/AuthserverApplication.java
+++ src/main/java/ru/kpfu/synergy/authserver/AuthserverApplication.java
@@ -2,12 +2,14 @@ package ru.kpfu.synergy.authserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@EnableEurekaClient
 @Configuration
 public class AuthserverApplication {
 
